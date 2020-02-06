0cdd640627ca58a04c34d332978e50524d9e6d9c
matmaly <mateuszfmaly@gmail.com>
2020-02-06 16:57:17 +0000

    Cleaning up the code a bit

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -7,27 +7,26 @@ My Attempt at a binary search algorithm using oo
 import random
 
 class Binary_Search:
-	def __init__(self):
-		self.list_to_search = []
+    def __init__(self):	    
+        self.list_to_search = []
 
-	def generate_list(self):
-		self.list_to_search = random.sorted(sample(range(101), 101))
-		print(self.list_to_search)
+    def generate_list(self):
+        self.list_to_search = random.sample(range(101), 101)
+        sort.self.listen_to_search
+        print(self.list_to_search)
 		
-	def search_list(self,to_find):
-		element = False
-		while not element:
-			half_point = len(self.list_to_search) // 2
-			if to_find == self.list_to_search[half_point]:
-				print(f"Found at position: {self.list_to_search.index(half_point)}")
-				break
-			elif to_find > self.list_to_search[half_point]:
-				print("More")
-			elif to_find < self.list_to_search[half_point]:
-				print("Less"
-			else:
-				break 	
-		print(half_point)
+    def search_list(self,to_find):
+        element = False
+        while not element:
+            half_point = len(self.list_to_search) // 2
+            if to_find == self.list_to_search[half_point]:
+                print(f"Found at position: {self.list_to_search.index(half_point)}")
+                element = True
+                break
+            elif to_find > self.list_to_search[half_point]:
+                print("More")
+            elif to_find < self.list_to_search[half_point]:
+                print("Less")
 
 
 search = Binary_Search()
