e7b6b5d2bf9ca2a1ba23155e8729396d24349121
Mateusz Maly <mateuszfmaly@gmail.com>
2020-02-06 16:12:25 +0000

    Adding initial version of the binary search

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3.6
+
+'''
+My Attempt at a binary search algorithm using oo
+'''
+
+import random
+
+class Binary_Search:
+	def __init__(self):
+		self.list_to_search = []
+
+	def generate_list(self):
+		self.list_to_search = random.sample(range(101), 101)
+
+	def search_list():
+	
