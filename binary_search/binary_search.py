5934dc2073c936c281c8ceaac490df161b4b68c0
Mateusz Maly <mateuszfmaly@gmail.com>
2020-02-06 16:42:18 +0000

    Adding more code

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -11,11 +11,23 @@ class Binary_Search:
 		self.list_to_search = []
 
 	def generate_list(self):
-		self.list_to_search = random.sample(range(101), 101)
-
-	def search_list(self):
+		self.list_to_search = random.sorted(sample(range(101), 101))
 		print(self.list_to_search)
-
+		
+	def search_list(self,to_find):
+		element = False
+		while not element:
+			half_point = len(self.list_to_search) // 2
+			if to_find == self.list_to_search[half_point]:
+				print(f"Found at position: {self.list_to_search.index(half_point)}")
+				break
+			elif to_find > self.list_to_search[half_point]:
+				print("More")
+			elif to_find < self.list_to_search[half_point]:
+				print("Less"
+			else:
+				break 	
+		print(half_point)
 
 
 search = Binary_Search()
