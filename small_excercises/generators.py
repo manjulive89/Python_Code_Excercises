ee97306f642ced0234513fbf39415004245a765a
matmaly <mateuszfmaly@gmail.com>
2020-04-21 16:33:50 +0000

    More stuff on generators

--- small_excercises/generators.py
+++ small_excercises/generators.py
@@ -10,23 +10,69 @@ def gen(n):
         yield x * 2
 
 # Followin for loop prints the numbers that were yielded by the function, otherwise print points to the object in memory
-for number in gen(10):
-    print(number)
+#for number in gen(10):
+#    print(number)
 
-g = gen(5)
+#g = gen(5)
 
 # Next keyword is used to iterate through the yielded values, it's not holding everythin in memory. It remembers which one it called before.
 # ints only
 
-print(next(g))
-print(next(g))
+#print(next(g))
+#print(next(g))
 
-s = "hello"
+#s = "hello"
 
 # iter() fun allows for strings to be iterated with the next()
-s_iter = iter(s)
+#s_iter = iter(s)
 
 # following will print h and e 
+#print(next(s_iter))
+#print(next(s_iter))
+
+# Print squares of numbers up to N using a generator
+def problem1(n):
+    for x in range(n):
+        yield x ** 2
+
+for number in problem1(10):
+    print(number)
+print("\n")
+
+# generator which takes in low and high values and then assigns a random number between 1 and 10 to n, then yields 'n' random numbers between low and high
+def problem2(low, high):
+    import random
+
+    n = random.randint(1,10)
+
+    for x in range(n):
+        yield random.randint(low,high)
+
+for number in problem2(5,8):
+    print(number)
+
+print("\n")
+
+# Following shows iter() fun which converts the string into an iterator
+s = "hello"
+s_iter = iter(s)
 print(next(s_iter))
 print(next(s_iter))
+print(next(s_iter))
+print(next(s_iter))
+print(next(s_iter))
+
+print("\n")
+'''
+Generator Comprehension:
+(<expression> for <var> in <iterable> if <condition>)
+
+'''
+even_gen = (i for i in range(11) if i % 2 ==0)
+
+for number in even_gen:
+    print(number)
+
+
+
 
